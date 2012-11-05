ea988ad3cc67ef107ce09fc582912e7fd2d9d8fb
federico.pecora <federico.pecora@ed67d276-4fd3-bfb6-86fb-179e958a0a54>
2012-11-05 00:37:30 +0000

    Cleaned up TestFuzzyAllenIntervalNetworkSolver stuff. --F.

--- src/main/java/examples/TestFuzzyAllenIntervalNetworkSolver.java
+++ src/main/java/examples/TestFuzzyAllenIntervalNetworkSolver.java
@@ -9,28 +9,36 @@ public class TestFuzzyAllenIntervalNetworkSolver {
 	
 	public static void main(String[] args) {
 		FuzzyAllenIntervalNetworkSolver solver = new FuzzyAllenIntervalNetworkSolver();
-		
-		SimpleAllenInterval sai0 = (SimpleAllenInterval)solver.createVariable("aComponent");
-		SimpleAllenInterval sai1 = (SimpleAllenInterval)solver.createVariable("aComponent");
-		
-		FuzzyAllenIntervalConstraint con0 = new FuzzyAllenIntervalConstraint(FuzzyAllenIntervalConstraint.Type.Before);
-		con0.setFrom(sai0);
-		con0.setTo(sai1);
+		SimpleAllenInterval act0 = (SimpleAllenInterval)solver.createVariable();
+		SimpleAllenInterval act1 = (SimpleAllenInterval)solver.createVariable();
+		SimpleAllenInterval act2 = (SimpleAllenInterval)solver.createVariable();
 
-		FuzzyAllenIntervalConstraint con1 = new FuzzyAllenIntervalConstraint(FuzzyAllenIntervalConstraint.Type.Before);
-		con1.setFrom(sai1);
-		con1.setTo(sai0);
-				
 		ConstraintNetwork.draw(solver.getConstraintNetwork());
 				
-		FuzzyAllenIntervalConstraint[] allConstraints = {con0,con1};
-		if (!solver.addConstraints(allConstraints)) { 
+		FuzzyAllenIntervalConstraint con0 = new FuzzyAllenIntervalConstraint(FuzzyAllenIntervalConstraint.Type.After);
+		con0.setFrom(act0);
+		con0.setTo(act1);
+		//if (!solver.addConstraint(con0)) System.out.println("Failed to add constraint " + con0);
+		
+		FuzzyAllenIntervalConstraint con1 = new FuzzyAllenIntervalConstraint(FuzzyAllenIntervalConstraint.Type.Contains);
+		con1.setFrom(act1);
+		con1.setTo(act2);
+		//if (!solver.addConstraint(con1)) System.out.println("Failed to add constraint " + con1);
+
+		FuzzyAllenIntervalConstraint con3 = new FuzzyAllenIntervalConstraint(FuzzyAllenIntervalConstraint.Type.Meets);
+		con3.setFrom(act2);
+		con3.setTo(act0);
+		//if (!solver.addConstraint(con3)) System.out.println("Failed to add constraint " + con2);
+
+		
+		FuzzyAllenIntervalConstraint[] allConstraints = {con0,con1,con3};
+		if (!solver.addConstraints(allConstraints)) {
 			System.out.println("Failed to add constraints!");
 			System.exit(0);
 		}
 		
+		System.out.println(solver.getPosibilityDegree());
 		
-		System.out.println("Poss: " + solver.getPosibilityDegree());
 		
 		
 	}
