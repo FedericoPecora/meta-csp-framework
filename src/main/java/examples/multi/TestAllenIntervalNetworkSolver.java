cb5f5b700adb70a6243dd58b042c2b981b23d4cc
federico.pecora <federico.pecora@ed67d276-4fd3-bfb6-86fb-179e958a0a54>
2013-01-29 18:11:16 +0000

    Restored TestAllenIntervalNetworkSolver. --F.

--- src/main/java/examples/multi/TestAllenIntervalNetworkSolver.java
+++ src/main/java/examples/multi/TestAllenIntervalNetworkSolver.java
@@ -1,91 +1,46 @@
-//package examples.multi;
-//
-//import java.util.logging.Level;
-//
-//import multi.allenInterval.AllenInterval;
-//import multi.allenInterval.AllenIntervalConstraint;
-//import multi.allenInterval.AllenIntervalNetworkSolver;
-//import time.APSPSolver;
-//import time.Bounds;
-//import utility.logging.MetaCSPLogging;
-//import framework.Constraint;
-//import framework.ConstraintNetwork;
-// 
-//public class TestAllenIntervalNetworkSolver {
-//	
-//	
-//	public static void main(String[] args) {
-//		
-//		System.out.println("fromScratch 1: " + APSPSolver.fromScratchCounter);
-//		AllenIntervalNetworkSolver solver = new AllenIntervalNetworkSolver(0, 100);
-//		System.out.println("fromScratch 2: " + APSPSolver.fromScratchCounter);
-//		AllenInterval[] intervals = (AllenInterval[])solver.createVariables(3);
-//		System.out.println("fromScratch 3: " + APSPSolver.fromScratchCounter);
-//
-//		AllenIntervalConstraint con1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, new Bounds(10, 20));
-//		con1.setFrom(intervals[0]);
-//		con1.setTo(intervals[1]);
-//		
-//		AllenIntervalConstraint con2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, new Bounds(10, 20));
-//		con2.setFrom(intervals[1]);
-//		con2.setTo(intervals[2]);
-//		AllenIntervalConstraint con3 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, AllenIntervalConstraint.Type.After.getDefaultBounds());
-//		con3.setFrom(intervals[2]);
-//		con3.setTo(intervals[0]);
-//		
-//		System.out.println( solver.addConstraint(con1) );
-//		
-//		System.out.println("fromScratch 4: " + APSPSolver.fromScratchCounter);
-//
-//		
-////		solver.printMatrix();
-//		System.out.println(solver.bookmark());
-//		
-//		System.out.println( solver.addConstraint(con2) );
-//		
-//		System.out.println("fromScratch 5: " + APSPSolver.fromScratchCounter);
-//
-//		
-////		solver.printMatrix();
-//		System.out.println(solver.bookmark());
-//		
-//		
-//		System.out.println( solver.addConstraint(con3) );
-//		
-//		System.out.println("fromScratch 6: " + APSPSolver.fromScratchCounter);
-//
-//		
-////		solver.printMatrix();
-//		solver.revert(1);
-////		System.out.println("..................................");
-////		solver.printMatrix();		
-//		System.out.println( solver.addConstraint(con3) );
-//		
-//		System.out.println("fromScratch 7: " + APSPSolver.fromScratchCounter);
-//
-//		
-//		solver.revert(0);
-////		solver.printMatrix();
-//		System.out.println( solver.addConstraint(con3) );
-//		
-//		System.out.println("fromScratch 8: " + APSPSolver.fromScratchCounter);
-//
-//		
-////		Constraint[] cons = new Constraint[]{con1,con2,con3};
-////		System.out.println(solver.addConstraintsDebug(cons) == null);
-//
-//		System.out.println("fromScratch 9: " + APSPSolver.fromScratchCounter);
-//		System.out.println("inc: " + APSPSolver.incCounter);
-//		System.out.println("singleTPCreate: " + APSPSolver.singleTPcreation);
-//		System.out.println("multiTOCreate: " + APSPSolver.multiTPcreation);
-//		System.out.println("tPDelete: " + APSPSolver.tpDeleteCount);
-//		System.out.println("cCreate: " + APSPSolver.cCreate);
-//		System.out.println("constructorCalls: " + APSPSolver.constructorCalls);
-//		System.out.println("propagations: " + APSPSolver.propagations);
-//		
-//		System.out.println("sCvar: " + APSPSolver.singleCreateVarSub);
-//		System.out.println("mCvar: " + APSPSolver.multiCreateVarSub);
-//
-//	}
-//
-//}
+package examples.multi;
+
+import java.util.logging.Level;
+
+import multi.allenInterval.AllenInterval;
+import multi.allenInterval.AllenIntervalConstraint;
+import multi.allenInterval.AllenIntervalNetworkSolver;
+import time.Bounds;
+import utility.logging.MetaCSPLogging;
+import framework.Constraint;
+import framework.ConstraintNetwork;
+
+public class TestAllenIntervalNetworkSolver {
+        
+        
+        public static void main(String[] args) {
+                
+                AllenIntervalNetworkSolver solver = new AllenIntervalNetworkSolver(0, 100);
+                AllenInterval[] intervals = (AllenInterval[])solver.createVariables(3);
+
+                MetaCSPLogging.setLevel(Level.FINEST);
+//              MetaCSPLogging.setLevel(solver.getClass(), Level.FINEST);
+//              MetaCSPLogging.setLevel(solver.getConstraintSolvers()[0].getClass(), Level.FINEST);
+                
+                //DRAW IT!
+                ConstraintNetwork.draw(solver.getConstraintNetwork());
+                
+                AllenIntervalConstraint con1 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.During, AllenIntervalConstraint.Type.During.getDefaultBounds());
+                con1.setFrom(intervals[0]);
+                con1.setTo(intervals[1]);
+                
+                AllenIntervalConstraint con2 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Duration, new Bounds(30, 40));
+                con2.setFrom(intervals[0]);
+                con2.setTo(intervals[0]);
+
+                AllenIntervalConstraint con3 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Overlaps, AllenIntervalConstraint.Type.Overlaps.getDefaultBounds());
+                con3.setFrom(intervals[1]);
+                con3.setTo(intervals[2]);
+
+                Constraint[] cons = new Constraint[]{con1,con2,con3};
+                System.out.println(solver.addConstraintsDebug(cons) == null);
+
+
+        }
+
+}
