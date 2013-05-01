3592a819625e24e947ed4fd6b649220683a07a21
iran.mansoori85@gmail.com <iran.mansoori85@gmail.com@ed67d276-4fd3-bfb6-86fb-179e958a0a54>
2013-05-01 15:29:04 +0000

    migration to new implementation of ARA wo culprit detection --iran

--- src/main/java/sandbox/spatial/rectangleAlgebra2/RectangleConstraintSolver2.java
+++ src/main/java/sandbox/spatial/rectangleAlgebra2/RectangleConstraintSolver2.java
@@ -95,41 +95,6 @@ public class RectangleConstraintSolver2 extends MultiConstraintSolver {
 	@Override
 	public boolean propagate() {
 		// Do nothing, AllenIntervalNetworkSolver takes care of propagation...
-		
-		
-//		if(this.getConstraints().length == 0) return true;
-//		for (int i = 0; i < this.getVariables().length; i++) {
-//			getVaribaleById.put(this.getVariables()[i].getID(), this.getVariables()[i]);
-//		}
-//		
-//		recRels = new Vector<Vector<RectangleConstraint2>>();
-//		recRels = createAllenCompleteNetwork(this.getConstraints());
-//		
-//		
-//		if(debug){
-//			boolean fail = true;
-//			System.out.println("Debug Mode:");
-//			System.out.println("Before: ");
-//			System.out.println(PrintSpatialRelation(recRels));
-//			
-//			
-//			if(weakPathConsistency(recRels))
-//				fail = false;
-//			
-//			System.out.println("After:");
-//			System.out.println(PrintSpatialRelation(recRels));
-//			if(fail) return false;
-//			else return true;
-//		}
-//		
-//		if(!debug){
-//			if(weakPathConsistency(recRels))
-//				return true;
-//			return false;
-//		}
-//		
-//		return false;
-
 		return true;
 	}
 
@@ -172,223 +137,21 @@ public class RectangleConstraintSolver2 extends MultiConstraintSolver {
 		return new BoundingBox(xLB, xUB, yLB, yUB);
 	}
 	
+	public BoundingBox extractBoundingBoxesFromSTPs(String name){
+		
+		Bounds xLB, xUB, yLB, yUB;
+		for (int i = 0; i < this.getConstraintSolvers()[0].getVariables().length; i++) {
+			if(((RectangularRegion2)this.getConstraintNetwork().getVariables()[i]).getName().compareTo(name) == 0){
+				xLB = new Bounds(((AllenInterval)this.getConstraintSolvers()[0].getVariables()[i]).getEST(), ((AllenInterval)this.getConstraintSolvers()[0].getVariables()[i]).getLST());
+				xUB = new Bounds(((AllenInterval)this.getConstraintSolvers()[0].getVariables()[i]).getEET(), ((AllenInterval)this.getConstraintSolvers()[0].getVariables()[i]).getLET());
+				yLB = new Bounds(((AllenInterval)this.getConstraintSolvers()[1].getVariables()[i]).getEST(), ((AllenInterval)this.getConstraintSolvers()[1].getVariables()[i]).getLST());
+				yUB = new Bounds(((AllenInterval)this.getConstraintSolvers()[1].getVariables()[i]).getEET(), ((AllenInterval)this.getConstraintSolvers()[1].getVariables()[i]).getLET());
+				return new BoundingBox(xLB, xUB, yLB, yUB);
+			}
+		}
+		return null;
+	}
 	
-	//Qualitative part
-//	private String PrintSpatialRelation(Vector<Vector<RectangleConstraint2>> recRels) {
-//		
-//		String ret = "";
-//		for(int i = 0; i < recRels.size(); i++){
-//			for(int j = 0; j < recRels.size(); j++){
-//					if(i == j) continue;
-//					ret += ( "\n"+ i + " --> " +  j + " :");
-//					String rectangleRel = "";
-//					if(recRels.get(i).get(j).getTypes().length != 0 && recRels.get(i).get(j).getTypes().length != 0){
-//						for (int j2 = 0; j2 < recRels.get(i).get(j).getTypes().length; j2++) {
-//							if(j2 > 0)
-//								rectangleRel += " v ";
-//							rectangleRel += " (" + recRels.get(i).get(j).getTypes()[j2].getAllenType()[0] + ", " +  recRels.get(i).get(j).getTypes()[j2].getAllenType()[1]+") " + " + " 
-//								+ RectangleConstraint2.getRCCConstraint(recRels.get(i).get(j).getTypes()[j2].getAllenType()[0], recRels.get(i).get(j).getTypes()[j2].getAllenType()[1]) + "  ";
-//						}
-//					}
-//					if(rectangleRel == "")
-//						rectangleRel = " no Constraint ";
-//					ret +=  recRels.get(i).get(j).getFrom() + rectangleRel + recRels.get(i).get(j).getTo() +"\n";
-//				}
-//		}
-//		return ret;
-//	}
-//
-//
-//	private boolean weakPathConsistency(Vector<Vector<RectangleConstraint2>> recRels) {
-//		int numVars = this.getVariables().length;
-//		//need to cycle at least (numVars^2 - numVars) times
-//		int counter = numVars*numVars - numVars;
-//		boolean[][] mark = new boolean[numVars][numVars];
-//		
-//		for(int i = 0; i < numVars; i++) {
-//			for(int j = 0; j < numVars; j++) {
-//				if(i == j) mark[i][j] = false;
-//				else mark[i][j] = true;
-//			}
-//		}	
-//		
-//		while(counter != 0) { // while the set is not empty
-//			for(int i = 0; i < numVars; i++) {
-//				for(int j = 0; j < numVars; j++) {
-//					if(i == j) continue;
-//					if(mark[i][j]){
-//						mark[i][j] = false;	
-//						counter--; //remove from set
-//						for(int k = 0; k < numVars; k++) {
-//							if(k == i || k == j) continue;
-//							//process relation (k,j)
-//							//back up relation (k,j)
-//							RectangleConstraint2 rectmp = (RectangleConstraint2)recRels.get(k).get(j).clone();
-//							//(k,j) <-- (k,j) n (k,i) + (i,j) 
-//							if(!revise(recRels.get(k).get(j), recRels.get(k).get(i), recRels.get(i).get(j)))
-//								return false;
-//
-//							//if changed, must re-process (k,j)
-//							
-//							if(!compareRelation(rectmp, recRels.get(k).get(j))) {
-//								
-//								if(!mark[k][j]) {
-//									mark[k][j] = true;
-//									counter++;
-//								}
-//							}
-//							//process relation (i,k)
-//							//back up relation (i,k)
-//							rectmp = (RectangleConstraint2)recRels.get(i).get(k).clone();
-//							//(i,k) <-- (i,k) n (i,j) + (j,k)
-//							
-//							if(!revise(recRels.get(i).get(k), recRels.get(i).get(j), recRels.get(j).get(k)))
-//								return false;
-//					
-//								
-//							
-//							//if changed, must re-process (i,k)
-//							if(!compareRelation(rectmp, recRels.get(i).get(k))) {
-//								if(!mark[i][k]) {
-//									mark[i][k] = true;
-//									counter++;
-//								}
-//							}
-//						}//end of k loop	
-//					}
-//				}
-//			}
-//		}
-//		return true;
-//	}
-//	
-//	//this is not true
-//	private boolean compareRelation(RectangleConstraint2 first, RectangleConstraint2 second) {
-//				
-//		boolean existed = false;
-//		for (int i = 0; i < first.types.length; i++) {
-//			existed = false;
-//			for (int j = 0; j < second.types.length; j++) {
-//				if(first.types[i].getAllenType()[0] == second.types[j].getAllenType()[0] && first.types[i].getAllenType()[1] == second.types[j].getAllenType()[1])
-//					existed = true;
-//			}
-//			if(!existed) return false;
-//		}
-//		return true;		
-//	}
-//
-//
-//	private boolean revise(RectangleConstraint2 originalRealtions, RectangleConstraint2 recCons, RectangleConstraint2 recCons2) {
-//	
-//		Vector<QualitativeAllenIntervalConstraint.Type> xcomRelation = new Vector<QualitativeAllenIntervalConstraint.Type>();
-//		Vector<QualitativeAllenIntervalConstraint.Type> ycomRelation = new Vector<QualitativeAllenIntervalConstraint.Type>();
-//		for (int t = 0; t < recCons.types.length; t++) {
-//			for (int t2 = 0; t2 < recCons2.types.length; t2++) {
-//				QualitativeAllenIntervalConstraint.Type[] xType = QualitativeAllenIntervalConstraint.transitionTable[recCons.types[t].getAllenType()[0].ordinal()][recCons2.types[t2].getAllenType()[0].ordinal()];
-//				QualitativeAllenIntervalConstraint.Type[] yType = QualitativeAllenIntervalConstraint.transitionTable[recCons.types[t].getAllenType()[1].ordinal()][recCons2.types[t2].getAllenType()[1].ordinal()];
-//				for(QualitativeAllenIntervalConstraint.Type t3: xType){
-//					if(!xcomRelation.contains(t3))				
-//						xcomRelation.add(t3);
-//				}
-//				for(QualitativeAllenIntervalConstraint.Type t3: yType){
-//						if(!ycomRelation.contains(t3))				
-//							ycomRelation.add(t3);
-//				}
-//			}
-//		}
-//		//zero for x and 1 for y
-//		Vector<Integer> xnonexists = getNonExistedIndices(originalRealtions, xcomRelation, 0);
-//		Vector<Integer> ynonexists = getNonExistedIndices(originalRealtions, ycomRelation, 1);
-//		
-//		//TwoDimensionsAllenConstraint[] t = new TwoDimensionsAllenConstraint[originalRealtions.getTypes().length - Math.min(xnonexists.size(), ynonexists.size())]; 
-//		Vector<TwoDimensionsAllenConstraint> t = new Vector<TwoDimensionsAllenConstraint>();
-//		
-//		for (int i = 0; i < originalRealtions.getTypes().length; i++) 
-//			if((!xnonexists.contains(i)) && (!ynonexists.contains(i)))
-//				t.add(originalRealtions.getTypes()[i]);
-//			
-//		originalRealtions.setTypes(t.toArray(new TwoDimensionsAllenConstraint[t.size()]));
-//		//if the result of intersection is empty 
-//		if(t.size() == 0) return false;
-//		
-//		// TODO Auto-generated method stub
-//		return true;
-//	}
-//
-//
-//
-//
-//	private Vector<Integer> getNonExistedIndices(
-//			RectangleConstraint2 originalRealtions, Vector<Type> comRelation, int dimension) {
-//		
-//		Vector<Integer> nonexists = new Vector<Integer>();
-//		boolean exist = false;
-//		for (int i = 0; i < originalRealtions.getTypes().length; i++) {
-//			exist = false;
-//			for (int j = 0; j < comRelation.size(); j++) {
-//				if(originalRealtions.getTypes()[i].getAllenType()[dimension].equals(comRelation.get(j))){
-//					exist = true;
-//					break;
-//				}
-//			}
-//			if(!exist) nonexists.add(i);
-//		}
-//		
-//		return nonexists;
-//
-//	}
-//
-//
-//	private Vector<Vector<RectangleConstraint2>> createAllenCompleteNetwork(Constraint[] c) {
-//
-//		RectangleConstraint2[][] tmp = new RectangleConstraint2[this.getVariables().length][this.getVariables().length];
-//		int row = 0 , col = 0;
-//		for(int i = 0; i < c.length; i++){
-//			row = this.getID(c[i].getScope()[0]);
-//			col = this.getID(c[i].getScope()[1]);
-//			tmp[row][col] = (RectangleConstraint2)c[i];
-//		}
-//		
-//		for(int i = 0; i < tmp.length; i++){
-//			Vector<RectangleConstraint2> con = new Vector<RectangleConstraint2>(); 
-//			for(int j = 0; j < tmp.length; j++){
-//				if(tmp[i][j] != null)  						
-//					con.add(tmp[i][j]);
-//				else if (tmp[j][i] != null){
-//					Vector<TwoDimensionsAllenConstraint> invers2DAllenRelains = new Vector<TwoDimensionsAllenConstraint>();
-//					for (int k = 0; k < tmp[j][i].getTypes().length; k++) {
-//						invers2DAllenRelains.add(new TwoDimensionsAllenConstraint(QualitativeAllenIntervalConstraint.getInverseRelation(tmp[j][i].types[k].getAllenType()[0]), QualitativeAllenIntervalConstraint.getInverseRelation(tmp[j][i].types[k].getAllenType()[1])));
-//					}
-//					RectangleConstraint2 rc = new RectangleConstraint2(invers2DAllenRelains.toArray(new TwoDimensionsAllenConstraint[invers2DAllenRelains.size()]));
-//					rc.setFrom(getVaribaleById.get(i));
-//					rc.setTo(getVaribaleById.get(j));
-//					con.add(rc);	
-//				}
-//				//if no relation exists
-//				else{
-//					RectangleConstraint2 universe = createUnivreseRectangleRelation();
-//					universe.setFrom(getVaribaleById.get(i));
-//					universe.setTo(getVaribaleById.get(j));
-//					con.add(universe);
-//				}
-//			}
-//			recRels.add(con);
-//		}
-//		return recRels;
-//	}
-//
-//
-//	private RectangleConstraint2 createUnivreseRectangleRelation() {
-//		
-//		Vector<TwoDimensionsAllenConstraint> tdr = new Vector<TwoDimensionsAllenConstraint>();  
-//		for (int i = 0; i < QualitativeAllenIntervalConstraint.Type.values().length; i++) 
-//			for (int j = 0; j < QualitativeAllenIntervalConstraint.Type.values().length; j++)
-//				tdr.add(new TwoDimensionsAllenConstraint(QualitativeAllenIntervalConstraint.Type.values()[i], QualitativeAllenIntervalConstraint.Type.values()[j]));
-//
-//		return new RectangleConstraint2(tdr.toArray(new TwoDimensionsAllenConstraint[tdr.size()]));
-//	}	
-
-
 
 }
 	
