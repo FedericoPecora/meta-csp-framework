2bb7d0b7f5c9d9f42c1013e56e086a65d5a7ead5
federico.pecora@gmail.com <federico.pecora@gmail.com@ed67d276-4fd3-bfb6-86fb-179e958a0a54>
2013-08-08 16:36:55 +0000

    Added script to remove old javadoc entries. --F.

---
+++ remove-old-javadoc-entries.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+files=`find javadoc/ -name [A-Z]*.html`
+for filename in $files
+do
+    srcName=${filename/javadoc/trunk\/src\/main\/java}
+    srcName=${srcName%.*}".java"
+    if [ ! -f $srcName ]; then
+	num=`echo "$srcName" | grep -o "\." | wc -l`
+	if [ $num == 1 ]; then
+	    echo $srcName" not found!"
+	    echo "Removing "$filename
+	    rm $filename
+	fi
+    fi
+	
+done
