5c87a1c47fe73250f539e691959a84a2695ebf34
iran.mansoori85@gmail.com <iran.mansoori85@gmail.com@ed67d276-4fd3-bfb6-86fb-179e958a0a54>
2014-02-06 14:03:03 +0000

    sensingScheduable Added --iran

---
+++ src/main/java/org/metacsp/meta/hybridPlanner/SensingSchedulable.java
@@ -0,0 +1,164 @@
+package org.metacsp.meta.hybridPlanner;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+import org.metacsp.framework.Constraint;
+import org.metacsp.framework.ConstraintNetwork;
+import org.metacsp.framework.ConstraintSolver;
+import org.metacsp.framework.ValueOrderingH;
+import org.metacsp.framework.VariableOrderingH;
+import org.metacsp.framework.meta.MetaConstraint;
+import org.metacsp.framework.meta.MetaVariable;
+import org.metacsp.multi.activity.Activity;
+import org.metacsp.multi.allenInterval.AllenIntervalConstraint;
+import org.metacsp.multi.spatioTemporal.SpatialFluent;
+import org.metacsp.multi.spatioTemporal.SpatialFluentSolver;
+import org.metacsp.time.APSPSolver;
+import org.metacsp.time.Bounds;
+
+public class SensingSchedulable extends MetaConstraint{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5232587914603939099L;
+	long beforeParameter = 1;
+	public SensingSchedulable(VariableOrderingH varOH, ValueOrderingH valOH) {
+		super(varOH, valOH);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	private ConstraintNetwork[] binaryPeakCollection(Vector<Activity> activities) {
+		
+		if (activities != null && !activities.isEmpty()) {
+			Vector<ConstraintNetwork> ret = new Vector<ConstraintNetwork>();
+			logger.finest("Doing binary peak collection with " + activities.size() + " activities...");
+			Activity[] groundVars = activities.toArray(new Activity[activities.size()]);
+			
+//			for (Activity a : groundVars) {
+//				if (isConflicting(new Activity[] {a})) {
+//					ConstraintNetwork cn = new ConstraintNetwork(null);
+//					cn.addVariable(a);
+//					ret.add(cn);
+//				}
+//			}
+			if (!ret.isEmpty()) {
+				return ret.toArray(new ConstraintNetwork[ret.size()]);
+			}
+			for (int i = 0; i < groundVars.length-1; i++) {
+				for (int j = i+1; j < groundVars.length; j++) {
+					Bounds bi = new Bounds(groundVars[i].getTemporalVariable().getEST(), groundVars[i].getTemporalVariable().getEET());
+					Bounds bj = new Bounds(groundVars[j].getTemporalVariable().getEST(), groundVars[j].getTemporalVariable().getEET());
+					if (bi.intersectStrict(bj) != null) {
+						ConstraintNetwork cn = new ConstraintNetwork(null);
+						cn.addVariable(groundVars[i]);
+						cn.addVariable(groundVars[j]);
+						ret.add(cn);
+					}
+				}
+			}
+			if (!ret.isEmpty()) {
+				return ret.toArray(new ConstraintNetwork[ret.size()]);			
+			}
+		}
+		return (new ConstraintNetwork[0]);
+	}
+
+	@Override
+	public ConstraintNetwork[] getMetaVariables() {
+		
+		
+		
+		Vector<Activity> activities = new Vector<Activity>();
+		for (int i = 0; i < ((SpatialFluentSolver)this.metaCS.getConstraintSolvers()[0]).getConstraintSolvers()[1].getVariables().length; i++) {
+			Activity act = (Activity)((SpatialFluentSolver)this.metaCS.getConstraintSolvers()[0]).getConstraintSolvers()[1].getVariables()[i];			
+			if(act.getSymbolicVariable().getSymbols()[0].toString().contains("sens")){
+				activities.add(act);
+			}
+		}
+		
+		return binaryPeakCollection(activities);
+	}
+
+	@Override
+	public ConstraintNetwork[] getMetaValues(MetaVariable metaVariable) {
+
+		ConstraintNetwork conflict = metaVariable.getConstraintNetwork();
+		//we know that is the result of binary conflict! so it is safe not to enumerate all, and hard coded
+		Vector<ConstraintNetwork> ret = new Vector<ConstraintNetwork>();
+		
+		AllenIntervalConstraint before01 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, new Bounds(beforeParameter, APSPSolver.INF));
+		before01.setFrom((Activity) conflict.getVariables()[0]);			
+		before01.setTo((Activity) conflict.getVariables()[1]);
+		ConstraintNetwork resolver0 = new ConstraintNetwork(((SpatialFluentSolver)this.metaCS.getConstraintSolvers()[0]).getConstraintSolvers()[1]);
+		resolver0.addVariable((Activity) conflict.getVariables()[0]);
+		resolver0.addVariable((Activity) conflict.getVariables()[1]);
+		resolver0.addConstraint(before01);
+		ret.add(resolver0);
+		
+		AllenIntervalConstraint before10 = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Before, new Bounds(beforeParameter, APSPSolver.INF));
+		before10.setFrom((Activity) conflict.getVariables()[1]);			
+		before10.setTo((Activity) conflict.getVariables()[0]);
+		ConstraintNetwork resolver = new ConstraintNetwork(((SpatialFluentSolver)this.metaCS.getConstraintSolvers()[0]).getConstraintSolvers()[1]);
+		resolver.addVariable((Activity) conflict.getVariables()[1]);
+		resolver.addVariable((Activity) conflict.getVariables()[0]);
+		resolver.addConstraint(before10);
+		ret.add(resolver);
+		
+		return ret.toArray(new ConstraintNetwork[ret.size()]);
+	
+	}
+
+	@Override
+	public ConstraintNetwork[] getMetaValues(MetaVariable metaVariable,
+			int initial_time) {
+		
+		return getMetaValues(metaVariable);
+	}
+
+	@Override
+	public void markResolvedSub(MetaVariable metaVariable,
+			ConstraintNetwork metaValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(ConstraintNetwork network) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ConstraintSolver getGroundSolver() {
+		
+		return ((SpatialFluentSolver)metaCS.getConstraintSolvers()[0]);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEdgeLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEquivalent(Constraint c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
