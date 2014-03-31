a93484335170b04ed0bd929db6ac54f42df3b122
mauriziodirocco@gmail.com <mauriziodirocco@gmail.com@ed67d276-4fd3-bfb6-86fb-179e958a0a54>
2014-03-31 14:46:37 +0000

    Added in MetaConstraintSolver the inverse mapping of solvers ( <conflict,value> map) field, i.e. <value,conflict> mapping

--- src/main/java/org/metacsp/framework/ConstraintNetworkMarking.java
+++ src/main/java/org/metacsp/framework/ConstraintNetworkMarking.java
@@ -1,11 +1,21 @@
 package org.metacsp.framework;
 
+
+
 public class ConstraintNetworkMarking {
 	
 	protected String networkState;
+
+	// the state has to be replaced by this
+	public enum markings{OBSERVABLE,IMPOSSIBLE,NONE}
+
 	
 	public ConstraintNetworkMarking() {
 		this.networkState="NONE";
+	}
+	
+	public ConstraintNetworkMarking(String state){
+		this.networkState=state;
 	}
 	
 	public String getState(){return this.networkState;}
