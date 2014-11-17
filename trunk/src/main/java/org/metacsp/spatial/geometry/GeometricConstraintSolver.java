4554886f74bbfeb2da67d207c518304edbcdae74
iran.mansoori85 <iran.mansoori85@ed67d276-4fd3-bfb6-86fb-179e958a0a54>
2014-11-17 23:12:54 +0000

    verify if firt polygon is not movable --iran

--- trunk/src/main/java/org/metacsp/spatial/geometry/GeometricConstraintSolver.java
+++ trunk/src/main/java/org/metacsp/spatial/geometry/GeometricConstraintSolver.java
@@ -10,15 +10,15 @@ import org.metacsp.framework.Variable;
 public class GeometricConstraintSolver extends RCC2ConstraintSolver{
 
 	private static final long serialVersionUID = -1610027841057830885L;
-	
+
 	//Min and max dimensions of the Eucledian space
 	public static final float MAX_X = 10000.0f;
 	public static final float MAX_Y = 10000.0f;
 	public static final float MIN_X = -10000.0f;
 	public static final float MIN_Y = -10000.0f;
-	
+
 	private HashMap<GeometricConstraint, HashMap<Polygon, Vec2[]>> constraintTrack = new HashMap<GeometricConstraint, HashMap<Polygon,Vec2[]>>();
-	
+
 	public GeometricConstraintSolver() {
 		//super(new Class[]{GeometricConstraint.class}, Polygon.class);
 		super();
@@ -30,45 +30,47 @@ public class GeometricConstraintSolver extends RCC2ConstraintSolver{
 		if(!super.propagate()) return false;
 		Constraint[] cons = this.getConstraints();	
 		for (int i = 0; i < cons.length; i++) {
-			if(!cons[i].isMasked()){
-				if(!super.propagate()) return false;
-				if(((GeometricConstraint)cons[i]).getType().equals(GeometricConstraint.Type.DC)){
-					Manifold manifold = new Manifold((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
-					if(manifold.isCollided()){
-						System.out.println("PROPAGATED DC between Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getFrom()).getID() + " Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getTo()).getID());
-						applyPolygonSeparation((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
-						
-					}				
-				}else if(((GeometricConstraint)cons[i]).getType().equals(GeometricConstraint.Type.INSIDE)){
-					Manifold manifold = new Manifold((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
-					//if(!manifold.isCollided()){
-						applyInside((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
-						System.out.println("PROPAGATED INSIDE between Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getFrom()).getID() + " Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getTo()).getID());
-					//}
+			if(!super.propagate()) return false;
+			if(((GeometricConstraint)cons[i]).getType().equals(GeometricConstraint.Type.DC)){
+				Manifold manifold = new Manifold((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
+				if(manifold.isCollided()){
+					System.out.println("PROPAGATED DC between Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getFrom()).getID() + " Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getTo()).getID());
+					applyPolygonSeparation((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
 				}				
-			}
+			}else if(((GeometricConstraint)cons[i]).getType().equals(GeometricConstraint.Type.INSIDE)){
+				Manifold manifold = new Manifold((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
+				//if(!manifold.isCollided()){
+				applyInside((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
+				System.out.println("PROPAGATED INSIDE between Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getFrom()).getID() + " Polygon " + ((Polygon)((GeometricConstraint)cons[i]).getTo()).getID());
+				//}
+			}				
 		}		
 		return true;
 	}
 
+	
+	private boolean checkInsideWithoutUpdate(Polygon p1, Polygon p2) {
+		float EPSILON = 0.0003f;
+		SutherlandHodgman slh = new SutherlandHodgman(p1, p2);
+		for (int i = 0; i < p1.getFullSpaceRepresentation().size(); i++) {
+			if(Math.abs(p1.getFullSpaceRepresentation().get(i).x - slh.getClippedResult()[i].x) < EPSILON &&
+					Math.abs(p1.getFullSpaceRepresentation().get(i).y - slh.getClippedResult()[i].y) < EPSILON)
+				return true;
+		}
+		return false;			
+	}
+	
 	private void applyInside(Polygon p1, Polygon p2) {
-//		Manifold manifold = new Manifold(p1, p2);
-		//if p1 and p2 are separated change the domain of p1 equal to p2		
-//		if(!manifold.solve()){			
-			float difx = p2.getPosition().x - p1.getPosition().x ;
-			float dify = p2.getPosition().y - p1.getPosition().y ;
-			Vec2[] p1Domain = new Vec2[p1.getFullSpaceRepresentation().size()];
-			for (int i = 0; i < p1.getFullSpaceRepresentation().size(); i++) {
-				p1Domain[i] = new Vec2(p1.getFullSpaceRepresentation().get(i).x + difx,
-						p1.getFullSpaceRepresentation().get(i).y + dify);
-			}
-			p1.setDomain(p1Domain);
-			SutherlandHodgman slh = new SutherlandHodgman(p1, p2);
-			p1.setDomain(slh.getClippedResult());			
-//		}else{ 		//else if they are intersected p1 has to the result of intersection
-//			SutherlandHodgman slh = new SutherlandHodgman(p1, p2);
-//			p1.setDomain(slh.getClippedResult());
-//		}
+		float difx = p2.getPosition().x - p1.getPosition().x ;
+		float dify = p2.getPosition().y - p1.getPosition().y ;
+		Vec2[] p1Domain = new Vec2[p1.getFullSpaceRepresentation().size()];
+		for (int i = 0; i < p1.getFullSpaceRepresentation().size(); i++) {
+			p1Domain[i] = new Vec2(p1.getFullSpaceRepresentation().get(i).x + difx,
+					p1.getFullSpaceRepresentation().get(i).y + dify);
+		}
+		p1.setDomain(p1Domain);
+		SutherlandHodgman slh = new SutherlandHodgman(p1, p2);
+		p1.setDomain(slh.getClippedResult());			
 	}
 
 	private void applyPolygonSeparation(Polygon p1, Polygon p2) {
@@ -85,7 +87,16 @@ public class GeometricConstraintSolver extends RCC2ConstraintSolver{
 		for (int i = 0; i < cons.length; i++) {
 			//handling movable property
 			if(!((Polygon)((GeometricConstraint)cons[i]).getFrom()).isMovable())
-				return false;			
+				return verifySituation(cons[i]);
+			//if there is already any constraint between scopes of the added constraint reject the constraint			
+			if(this.getConstraints((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo()).length > 0){
+				if(((GeometricConstraint)(this.getConstraints((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo())[0])).getType().equals((GeometricConstraint)c[i]))
+					return true;
+				else
+					return false;
+				//				this.getConstraints((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo())[0].mask();
+				//				System.out.println("MAKSED CONSTRAINT: " + this.getConstraints((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo())[0]);
+			}
 			//keep track of added constraint for removing. i.e, taking snapshot of current domain of the vars
 			HashMap<Polygon, Vec2[]> poly2Domain = new HashMap<Polygon, Vec2[]>();
 			for (int j = 0; j < this.getVariables().length; j++) {
@@ -93,12 +104,6 @@ public class GeometricConstraintSolver extends RCC2ConstraintSolver{
 						.toArray(new Vec2[((Polygon)this.getVariables()[j]).getFullSpaceRepresentation().size()]));
 			}
 			constraintTrack.put((GeometricConstraint)c[i], poly2Domain);
-			//if there is already any constraint between scopes of the added constraint removes the constraint
-			
-			if(this.getConstraints((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo()).length > 0){
-				this.getConstraints((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo())[0].mask();
-				System.out.println("MAKSED CONSTRAINT: " + this.getConstraints((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo())[0]);
-			}
 			//adding constraint
 			if(((GeometricConstraint)cons[i]).getType().equals(GeometricConstraint.Type.DC)){	
 				applyPolygonSeparation((Polygon)((GeometricConstraint)cons[i]).getFrom(), (Polygon)((GeometricConstraint)cons[i]).getTo());
@@ -111,6 +116,21 @@ public class GeometricConstraintSolver extends RCC2ConstraintSolver{
 		return true;
 	}
 
+	private boolean verifySituation(Constraint c) {
+			if(((GeometricConstraint)c).getType().equals(GeometricConstraint.Type.DC)){
+				Manifold manifold = new Manifold((Polygon)((GeometricConstraint)c).getFrom(), (Polygon)((GeometricConstraint)c).getTo());
+				if(manifold.isCollided()){
+					return false;
+				}				
+			}else if(((GeometricConstraint)c).getType().equals(GeometricConstraint.Type.INSIDE)){
+				Manifold manifold = new Manifold((Polygon)((GeometricConstraint)c).getFrom(), (Polygon)((GeometricConstraint)c).getTo());
+//				if(manifold.isCollided())
+//					return false;
+				return checkInsideWithoutUpdate((Polygon)((GeometricConstraint)c).getFrom(), (Polygon)((GeometricConstraint)c).getTo());
+			}				
+		return false;
+	}
+
 	@Override
 	protected void removeConstraintsSub(Constraint[] c) {
 		for (int i = 0; i < c.length; i++) {
@@ -121,10 +141,10 @@ public class GeometricConstraintSolver extends RCC2ConstraintSolver{
 			}
 			constraintTrack.remove(c);
 		}
-		
+
 	}
 
-	
+
 	public static String drawPolygons(Vector<Vector<Vec2>> toPlots, long horizon){
 		String ret = "";
 		int j = 1;
@@ -144,6 +164,6 @@ public class GeometricConstraintSolver extends RCC2ConstraintSolver{
 		ret += "pause -1";
 		return ret;
 	}
-	
+
 
 }
