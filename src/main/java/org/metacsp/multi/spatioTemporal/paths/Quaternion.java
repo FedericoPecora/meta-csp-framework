312630195b009a09b4727195a20d8a8e1440abde
Federico Pecora <federico.pecora@oru.se>
2019-07-14 20:55:48 +0200

    Added Quaternion util functions

--- src/main/java/org/metacsp/multi/spatioTemporal/paths/Quaternion.java
+++ src/main/java/org/metacsp/multi/spatioTemporal/paths/Quaternion.java
@@ -123,5 +123,65 @@ public class Quaternion implements Serializable {
 	public double getW() {
 		return w;
 	}
+	
+	/**
+	 * Get the norm of this {@link Quaternion}.
+	 * @return The norm of this {@link Quaternion}.
+	 */
+	public double norm() {
+        return Math.sqrt(x*x + y*y + z*z + w*w);
+    }
+	
+	/**
+	 * Get the conjugate of this {@link Quaternion}.
+	 * @return The conjugate of this {@link Quaternion}.
+	 */
+	public Quaternion conjugate() {
+        return new Quaternion(x, -y, -z, -w);
+    }
+	
+	/**
+	 * Add this and another {@link Quaternion}s.
+	 * @param other A {@link Quaternion} to add to this {@link Quaternion}.
+	 * @return The sum of this and the given {@link Quaternion}.
+	 */
+	public Quaternion plus(Quaternion other) {
+        return new Quaternion(this.x+other.x, this.y+other.y, this.z+other.z, this.w+other.w);
+    }
+	
+	/**
+	 * Get a new {@link Quaternion} whose value is (<code>this</code> * <code>other</code>).
+	 * @param other A {@link Quaternion} to multiply by.
+	 * @return A new {@link Quaternion} whose value is (<code>this</code> * <code>other</code>).
+	 */
+	public Quaternion times(Quaternion other) {
+        double x = this.x*other.x - this.y*other.y - this.z*other.z - this.w*other.w;
+        double y = this.x*other.y + this.y*other.x + this.z*other.w - this.w*other.z;
+        double z = this.x*other.z - this.y*other.w + this.z*other.x + this.w*other.y;
+        double w = this.x*other.w + this.y*other.z - this.z*other.y + this.w*other.x;
+        return new Quaternion(x,y,z,w);
+    }
+
+	/**
+	 * Get this inverse of this {@link Quaternion}.
+	 * @return The inverse of this {@link Quaternion}.
+	 */
+	public Quaternion inverse() {
+		double d = x*x + y*y + z*z + w*w;
+		return new Quaternion(x/d, -y/d, -z/d, -w/d);
+	}
+	
+	/**
+	 * Get a new {@link Quaternion} whose value is <code>this</code> * <code>inverse(other)</code>.
+	 * @param other A {@link Quaternion} to divide by.
+	 * @return A new {@link Quaternion} whose value is <code>this</code> * <code>inverse(other)</code>.
+	 */
+	public Quaternion divide(Quaternion other) {
+        return this.times(other.inverse());
+    }
+	
+	public String toString() {
+        return x + " + " + y + "i + " + z + "j + " + w + "k";
+    }
 
 }
